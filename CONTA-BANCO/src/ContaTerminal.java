f45eeaef071f152d089fef09d80f5beee47306ac
Guilherme Koch <guilherme.koch02@gmail.com>
2023-05-04 16:40:50 -0300

    ContaTerminal

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 public class ContaTerminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        try (Scanner ler = new Scanner(System.in)) {
+            int numeroConta, numeroAgencia, saldo;
+            String nomeCliente, sobrenomeCliente, nomeCompleto;
+
+            System.out.println("Bem vindo a sua Conta Bancaria\ndigite seus dados para realizar o cadastro:");
+
+            System.out.println("Número da Conta: ");
+            numeroConta = ler.nextInt();
+            System.out.println("Agencia: ");
+            numeroAgencia = ler.nextInt();
+            System.out.println("Nome: ");
+            nomeCliente = ler.next();
+            System.out.println("Sobrenome: ");
+            sobrenomeCliente = ler.next();
+            System.out.println("Seu Saldo: ");
+            saldo = ler.nextInt();
+
+            nomeCompleto = nomeCliente + " " + sobrenomeCliente;
+
+            System.out.println("Olá senhor(a) " + nomeCompleto + ", sua conta é " + numeroConta + ", o número da sua agencia é " + numeroAgencia + ", seu saldo atual é de " + saldo);
+        }
     }
 }
