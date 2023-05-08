22b180712133d892cb91c01ca5d8c35b179b36ee
Guilherme Koch <129979302+GuilhermeKoch@users.noreply.github.com>
2023-05-08 11:40:01 -0300

    Delete DESAFIO-CONTROLE-FLUXO directory

--- DESAFIO-CONTROLE-FLUXO/src/ParametrosInvalidosException.java
+++
@@ -1,7 +0,0 @@
-public class ParametrosInvalidosException extends Exception{
-static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-    if (parametroUm > parametroDois){
-        throw new ParametrosInvalidosException();
-        }
-    }
-}
